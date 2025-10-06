e62df51ba716160c7e892d3277f43a02ea6bc9bb
SupernalAunt <burningblade5709@gmail.com>
2025-10-06 18:13:35 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOwtZ9hlq2tjh-wHyBtEF5gFpMMZQl0so",
+  authDomain: "ecdp-web.firebaseapp.com",
+  projectId: "ecdp-web",
+  storageBucket: "ecdp-web.firebasestorage.app",
+  messagingSenderId: "52884539513",
+  appId: "1:52884539513:web:90fb6a60e1699196152ca8",
+  measurementId: "G-6GP618SEYW"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+const analytics = getAnalytics(app);
+
+export { auth, db, analytics };
